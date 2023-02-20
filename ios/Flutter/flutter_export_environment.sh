4277c56ad000aecdff1afd7247a6902badc31f44
Yashas S <yashas.s2001@gmail.com>
2023-02-20 15:13:50 +0530

    gradle upgrade

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\College\6th sem\p-1\flutter_MakeMyTrip_UI_Demo"
+export "FLUTTER_APPLICATION_PATH=D:\Computer\Assignments\TravelApp\flutter_MakeMyTrip_UI_Demo"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
