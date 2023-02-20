eaa765e363c1385408d3a32e5cfd8198d245977c
Suhas M L <suhasml37@gmail.com>
2023-02-20 20:13:04 +0530

    changes

--- android/app/src/main/java/com/example/make_my_trip_flutter_ui/MainActivity.java
+++ android/app/src/main/java/com/example/make_my_trip_flutter_ui/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.make_my_trip_flutter_ui;
 
-import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+
+    @Override
+    public void configureFlutterEngine(FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
